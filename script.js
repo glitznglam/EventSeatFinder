d9a62b7572a19d5efcea915aa15e4654ae2b94fe
glitznglam <glitznglampartydeco@gmail.com>
2026-06-24 12:31:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-const guests = [
-  { name: "Angela Reyes", table: "2" },
-  { name: "Angelica Ramos", table: "7" },
-  { name: "Maria Santos", table: "5" },
-  { name: "John Cruz", table: "8" },
-  { name: "Mark Santos", table: "3" },
-  { name: "Maya Johnson", table: "6" }
-];
+let guests = [];
 
 const input = document.getElementById("guestName");
 const suggestionsBox = document.getElementById("suggestions");
+
+const sheetID = "18vHPjaoOreO1j6DnmZ9VYWS4q0p387HmrpqvmYUkTbU";
+
+const url =
+`https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?tqx=out:json`;
+
+fetch(url)
+.then(response => response.text())
+.then(data => {
+
+    const json = JSON.parse(
+        data.substring(47).slice(0, -2)
+    );
+
+    const rows = json.table.rows;
+
+    guests = rows.map(row => ({
+        name: row.c[0]?.v || "",
+        table: row.c[1]?.v || ""
+    }));
+
+})
+.catch(error => {
+    console.error("Sheet loading error:", error);
+});
 
 input.addEventListener("input", function () {
 
@@ -46,6 +64,7 @@ input.addEventListener("input", function () {
     });
 
     suggestionsBox.style.display = "block";
+
 });
 
 document.addEventListener("click", function (e) {
@@ -58,7 +77,7 @@ document.addEventListener("click", function (e) {
 
 function findSeat() {
 
-    const name = input.value.trim();
+    const name = input.value;
 
     const guest = guests.find(
         g => g.name.toLowerCase() === name.toLowerCase()
@@ -66,15 +85,17 @@ function findSeat() {
 
     if (guest) {
 
-        document.querySelector(".container").style.display = "none";
-
-        document.getElementById("resultCard").style.display = "block";
-
         document.getElementById("guestDisplay").innerHTML =
             guest.name;
 
         document.getElementById("tableDisplay").innerHTML =
             "You are seated at Table " + guest.table;
+
+        document.querySelector(".search-container").style.display =
+            "none";
+
+        document.getElementById("resultCard").style.display =
+            "block";
 
     } else {
 
@@ -85,10 +106,12 @@ function findSeat() {
 
 function closeResult() {
 
-    document.getElementById("resultCard").style.display = "none";
+    document.getElementById("resultCard").style.display =
+        "none";
 
-    document.querySelector(".container").style.display = "block";
+    document.querySelector(".search-container").style.display =
+        "block";
 
-    document.getElementById("guestName").value = "";
+    input.value = "";
 
 }
